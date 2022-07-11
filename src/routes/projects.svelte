605a6eb90106876fa1a38888e8dbfb7e67a9a633
Sam Graham <sgraham@cloudbees.com>
2022-07-11 13:37:27 -0400

    preload projects page images

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -13,19 +13,19 @@
 	}
 	const ariaSrc = './aria.png';
 	const httpBinSrc = './httpBin.png';
+  const scalePic = 'transition-all ease-in duration-300 hover:scale-110 hover:drop-shadow-2xl';
 </script>
 
-{#await preload(ariaSrc, httpBinSrc) then _}
 	<div class="flex flex-grow w-prose max-w-prose mx-auto">
 		<div class="flex flex-wrap mx-auto gap-x-16 gap-y-4 place-content-evenly">
 			<div class="w-72 pt-5 flex flex-col rounded-2xl">
-				<a href="https://aria-deploy.github.io/">
+				<!-- <a href="https://aria-deploy.github.io/"> -->
 					<img
-						class="rounded-2xl transition-all ease-in duration-300 hover:scale-110 hover:drop-shadow-2xl"
+						class="rounded-2xl"
 						alt="aria demo"
-						src="./aria.png"
+						src={ariaSrc}
 					/>
-				</a>
+				<!-- </a> -->
 				<p class="text-gray-600 pl-6 pr-3 text-[15px]">
 					<a class="italic font-bold" href="https://aria-deploy.github.io/"> aria </a>
 					is a tool that automates canary deployments apps to AWS, including monitoring. Not much going
@@ -33,13 +33,13 @@
 				</p>
 			</div>
 			<div class="w-72 flex flex-col items-center rounded-2xl">
-				<a href="https://github.com/SamGrah/httpBin">
+				<!-- <a href="https://github.com/SamGrah/httpBin"> -->
 					<img
-						class="w-[14.65rem] pt-5 transition-all ease-in duration-300 hover:scale-110 hover:drop-shadow-2xl"
+						class="w-[14.65rem] pt-5"
 						alt="httpBin homepage"
-						src="./httpBin.png"
+						src={httpBinSrc}
 					/>
-				</a>
+				<!-- </a> -->
 				<p class="text-gray-600 pl-8 text-[15px]">
 					<a class="italic font-bold" href="https://github.com/SamGrah/httpBin"> httpBin </a>
 					is a webhook testing app utilizing websockets. Currently migrating to SvelteKit, this project
@@ -48,4 +48,3 @@
 			</div>
 		</div>
 	</div>
-{/await}

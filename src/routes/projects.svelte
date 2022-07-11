c6665067481ca52f7ca1af1567ea08d6714db8e3
Sam Graham <sgraham@cloudbees.com>
2022-07-11 14:15:40 -0400

    fade in projects images

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -1,45 +1,31 @@
 <script>
-	function preload(...args) {
-		console.log(args);
-		return Promise.all( 
-			args.map(
-				(ref) =>
-					new Promise((resolve) => {
-						let img = new Image();
-						img.onload = resolve;
-						img.src = ref;
-					})
-			)
-		);
-	}
+	import { fade } from 'svelte/transition';
 	const ariaSrc = './aria.png';
 	const httpBinSrc = './httpBin.png';
-	const scalePic = 'transition-all ease-in duration-300 hover:scale-110 hover:drop-shadow-2xl';
+	const scalePic = 'transition-all ease-in duration-300 hover:scale-[1.01] hover:drop-shadow-2xl';
 </script>
 
-{#await preload(ariaSrc, httpBinSrc) then _}
-	<div class="flex flex-grow w-prose max-w-prose mx-auto">
-		<div class="flex flex-wrap mx-auto gap-x-16 gap-y-4 place-content-evenly">
-			<div class="w-72 pt-5 flex flex-col rounded-2xl">
-				<a href="https://aria-deploy.github.io/">
-				<img class="rounded-2xl" alt="aria demo" src={ariaSrc} />
-				</a>
-				<p class="text-gray-600 pl-6 pr-3 text-[15px]">
-					<a class="italic font-bold" href="https://aria-deploy.github.io/"> aria </a>
-					is a tool that automates canary deployments apps to AWS, including monitoring. Not much going
-					on with this project.
-				</p>
-			</div>
-			<div class="w-72 flex flex-col items-center rounded-2xl">
-				<a href="https://github.com/SamGrah/httpBin">
-				<img class="w-[14.65rem] pt-5" alt="httpBin homepage" src={httpBinSrc} />
-				</a>
-				<p class="text-gray-600 pl-8 text-[15px]">
-					<a class="italic font-bold" href="https://github.com/SamGrah/httpBin"> httpBin </a>
-					is a webhook testing app utilizing websockets. Currently migrating to SvelteKit, this project
-					is fodder for learning new tooling.
-				</p>
-			</div>
+<div class="flex flex-grow w-prose max-w-prose mx-auto">
+	<div class="flex flex-wrap mx-auto gap-x-16 gap-y-4 place-content-evenly">
+		<div in:fade class="w-72 pt-5 flex flex-col rounded-2xl">
+			<a href="https://aria-deploy.github.io/">
+				<img class="rounded-2xl {scalePic}" alt="aria demo" src={ariaSrc} />
+			</a>
+			<p class="text-gray-600 pl-6 pr-3 text-[15px]">
+				<a class="italic font-bold" href="https://aria-deploy.github.io/"> aria </a>
+				is a tool that automates canary deployments apps to AWS, including monitoring. Not much going
+				on with this project.
+			</p>
+		</div>
+		<div in:fade class="w-72 flex flex-col items-center rounded-2xl">
+			<a href="https://github.com/SamGrah/httpBin">
+				<img class="w-[14.65rem] pt-5 {scalePic}" alt="httpBin homepage" src={httpBinSrc} />
+			</a>
+			<p class="text-gray-600 pl-8 text-[15px]">
+				<a class="italic font-bold" href="https://github.com/SamGrah/httpBin"> httpBin </a>
+				is a webhook testing app utilizing websockets. Currently migrating to SvelteKit, this project
+				is fodder for learning new tooling.
+			</p>
 		</div>
 	</div>
-{/await}
+</div>

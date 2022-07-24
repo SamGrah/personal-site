2a38645613a3309db16b5598d22133cc7dd71087
SamGrah <sgraham@cloudbees.com>
2022-07-24 19:12:04 -0400

    redirect root to /blogs

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,11 @@
-<h1 class="text-3xl font-bold">My Personal Site</h1>
+<script context="module">
+    export async function load() {
+        return {
+            status: 302,
+            redirect: "/blogs"
+        };
+    }
+</script>
+<!-- <h1 class="text-3xl font-bold">My Personal Site</h1>
 <br />
-<p><a class="underline text-blue-500" href="/blogs">Articles</a> rendered from markdown</p>
+<p><a class="underline text-blue-500" href="/blogs">Articles</a> rendered from markdown</p> -->
